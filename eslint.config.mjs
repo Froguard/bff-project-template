d3ab924fa792553776025e954c7d617539625602
froguard <figure_wf@163.com>
2026-07-10 21:29:34 +0800

    chore: update docs and lint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,14 @@ const eslintConfig = [
   {
     rules: {
       "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          argsIgnorePattern: "^_",
+          caughtErrorsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+        },
+      ],
     },
   },
   {
