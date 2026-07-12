43cfd727e7bce25cf159ef94b84d3154b57e926c
froguard <figure_wf@163.com>
2026-07-12 13:24:15 +0800

    chore: update dependencies and shared types

--- src/types/koa-router.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "@koa/router" {
-  export default class Router {
-    get(path: string, middleware: (...args: unknown[]) => unknown): this;
-    post(path: string, middleware: (...args: unknown[]) => unknown): this;
-    routes(): (...args: unknown[]) => unknown;
-    allowedMethods(): (...args: unknown[]) => unknown;
-  }
-}
